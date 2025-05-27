a8c3c7c0a8fad96345210fa18b207aab972ca753
jeremyp <jpekmez@gmail.com>
2025-05-27 14:58:29 +0200

    change k3s image

--- pkg/plugins/gateway/gateway_rsp_body.go
+++ pkg/plugins/gateway/gateway_rsp_body.go
@@ -36,14 +36,6 @@ import (
 )
 
 func (s *Server) HandleResponseBody(ctx context.Context, requestID string, req *extProcPb.ProcessingRequest, user utils.User, rpm int64, model string, stream bool, traceTerm int64, hasCompleted bool) (*extProcPb.ProcessingResponse, bool) {
-	return &extProcPb.ProcessingResponse{
-		Response: &extProcPb.ProcessingResponse_ResponseBody{
-			ResponseBody: &extProcPb.BodyResponse{
-				Response: &extProcPb.CommonResponse{}, // No modifications to the body
-			},
-		},
-	}, hasCompleted
-
 	b := req.Request.(*extProcPb.ProcessingRequest_ResponseBody)
 
 	var res openai.ChatCompletion
@@ -62,6 +54,14 @@ func (s *Server) HandleResponseBody(ctx context.Context, requestID string, req *
 			}
 		}
 	}()
+
+	return &extProcPb.ProcessingResponse{
+		Response: &extProcPb.ProcessingResponse_ResponseBody{
+			ResponseBody: &extProcPb.BodyResponse{
+				Response: &extProcPb.CommonResponse{}, // No modifications to the body
+			},
+		},
+	}, hasCompleted
 
 	if stream {
 		t := &http.Response{
