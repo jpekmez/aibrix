d3702f822a8a073068ca2af3ca24590259119d1d
jeremyp <jpekmez@gmail.com>
2025-05-27 14:58:29 +0200

    change k3s image

--- k3d/build.sh
+++ k3d/build.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-K3S_TAG=${K3S_TAG:="v1.28.8-k3s1"}
+K3S_TAG=${K3S_TAG:="v1.30.13-k3s1"}
 CUDA_TAG=${CUDA_TAG:="12.8.1-base-ubuntu22.04"}
 IMAGE_REGISTRY=${IMAGE_REGISTRY:="us-central1-docker.pkg.dev/cohere-artifacts/cohere"}
 IMAGE_REPOSITORY=${IMAGE_REPOSITORY:="jeremy/k3d-gpu"}
